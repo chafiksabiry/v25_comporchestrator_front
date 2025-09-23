44e4b04c986737c62594ee0a21ef612cf542c63f
El Houcine Qara <elhoucine.qara@harx.ai>
2025-09-23 21:45:42 +0100

    Edit dockerfile

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
-    base: 'https://preprod-comp-orchestrator.harx.ai/',
+    base: 'https://comp-orchestrator.harx.ai/',
     plugins: [
       react({
         jsxRuntime: 'classic',
